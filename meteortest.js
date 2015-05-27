16a78a9d0766020d1c56e9171a8dcda6cf79eee6
Sirius <sirius@Siriuss-MacBook-Air.local>
2015-05-27 08:56:27 -0700

    Initial commit.

---
+++ meteortest.js
@@ -0,0 +1,89 @@
+Tiles = new Mongo.Collection("tiles"); 
+if (Meteor.isClient) {
+  // counter starts at 0
+/*
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function () {
+      return Session.get('counter');
+    }
+  });
+
+  Template.hello.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+*/
+
+
+
+
+
+
+  Template.body.helpers({
+    tiles: function () {
+      return Tiles.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+
+
+
+  Template.body.events({
+    'click': function (a, b) { 
+      var x = a.pageX,
+          y = a.pageY;
+
+      var grid = pixel2grid(x, y, 0);
+
+      console.log(x, y);
+
+      Tiles.insert({
+        x: x,
+        y: y,
+        type: 'cobblestone'
+      });
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
+
+
+
+
+// Helper functions
+var Tile = {
+  width: 128,
+  height: 96,
+  surface: 64,
+  elevation: 32,
+
+  offset: [
+    0,
+    -25 
+  ]
+}
+
+function pixel2grid (x, y, height) {
+  var i, j;
+  y += height * Tile.elevation + Tile.offset[1];
+  i = Math.floor(x / Tile.width - y / Tile.surface);
+  j = Math.floor(x / Tile.width + y / Tile.surface);
+  return [i, j];
+}
+
+function grid2pixel(i, j, height) { 
+  var x, y;
+  x = i * Tile.width / 2  + j * Tile.width / 2;
+  y = -i * Tile.top / 2 + j * Tile.top / 2;
+  y -= height * Tile.elevation;
+  return [x, y]; 
+}
